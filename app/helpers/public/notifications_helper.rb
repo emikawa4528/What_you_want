478659755c9161f37c51b676c24e6fe96c4c7f9d
emikawa4528 <emi.kawa.4528@gmail.com>
2023-10-15 06:58:33 +0000

    post

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -10,13 +10,13 @@ module Public::NotificationsHelper
       when "follow" then
         tag.a(notification.visitor.name, href: member_path(@visitor), style: "font-weight: bold;")+"さんがあなたをフォローしました。"
       when "like" then
-        tag.a(notification.visitor.name, href: member_path(@visitor), style: "font-weight: bold;")+"さんが"+tag.a("あなたの投稿", href: post_path(notification.post_id), style: "font-weight: bold;")+"にいいねしました。"
+        tag.a(notification.visitor.name, href: member_path(@visitor), style: "font-weight: bold;")+"さんが"+tag.a("あなたの投稿", href: idea_path(notification.idea_id), style: "font-weight: bold;")+"にいいねしました。"
       when "comment" then
         @comment = Comment.find_by(id: @visitor_comment)&.comment
-        if notification.post.member_id == @visited.id
-          tag.a(@visitor.name, href: member_path(@visitor), style: "font-weight: bold;")+"さんが"+tag.a("あなたの投稿", href: post_path(notification.post_id), style: "font-weight: bold;")+"にコメントしました。"
+        if notification.idea.member_id == @visited.id
+          tag.a(@visitor.name, href: member_path(@visitor), style: "font-weight: bold;")+"さんが"+tag.a("あなたの投稿", href: idea_path(notification.idea_id), style: "font-weight: bold;")+"にコメントしました。"
         else
-          tag.a(@visitor.name, href: member_path(@visitor), style: "font-weight: bold;")+"さんが"+tag.a("#{notification.post.member.name}さんの投稿", href: post_path(notification.post_id), style: "font-weight: bold;")+"にコメントしました。"
+          tag.a(@visitor.name, href: member_path(@visitor), style: "font-weight: bold;")+"さんが"+tag.a("#{notification.idea.member.name}さんの投稿", href: idea_path(notification.idea_id), style: "font-weight: bold;")+"にコメントしました。"
         end
     end
   end
